817c9eb28479e62af2f40a6a277db322affd9409
NEBULA1978 <rpascualey@gmail.com>
2023-04-17 20:53:38 +0200

    Añado archivo users.py

--- users.py
+++ users.py
@@ -7,11 +7,12 @@ app = FastAPI()
 #  uvicorn users:app --reload
 
 
-@app.get("/users")
-async def users():
+@app.get("/usersjson")
+async def usersjson():
     return [
         {"name": "Brais", "surname": "moure", "url": "https://moure.com/python"},
         {"name": "Moure", "surname": "Dev", "url": "https://mouredev.com/python"},
         {"name": "Haskon", "surname": "Danilberg",
             "url": "https://heakon.com/python"}
     ]
+# VOY MINUTO 1h:56
